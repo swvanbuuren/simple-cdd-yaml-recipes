ffb39df2d462eece60262cf43e0b9b2b1544bb3d
Sietze van Buuren <s.van.buuren@gmail.com>
2023-03-19 08:52:22 +0100

    Udpate grub, add profile keyword
    - Update grub script
    - Add profile keywod to desktop recipe
    
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/configure_grub.sh
+++ scripts/configure_grub.sh
@@ -2,9 +2,7 @@
 grub2default=/etc/default/grub
 if [ -f "$grub2default" ]; then
     # remove keywords from CMDLINE_LINUX_DEFAULT
-    for keyword in "quiet splash"; do
-	sed -i "/GRUB_CMDLINE_LINUX_DEFAULT/s/$keyword//g" $grub2default
-    done
+    sed -i '/CMDLINE_LINUX_DEFAULT/c\CMDLINE_LINUX_DEFAULT=""' $grub2default
     # set timeout to zero
     sed -i '/GRUB_TIMEOUT/c\GRUB_TIMEOUT=0' $grub2default
     # ensure the updates go live

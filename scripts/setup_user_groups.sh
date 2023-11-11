e84cd767732d15b302557d3a1f19229686308863
Sietze van Buuren <s.van.buuren@gmail.com>
2023-11-11 16:12:29 +0100

    Include add user to groups script to base recipe
    - Add script that allows to add a user to defined groups
    - Script included in the base recipe
    
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

---
+++ scripts/setup_user_groups.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+USER="{{username}}"
+DEFGROUPS="{{groups}}"
+for group in $(echo $DEFGROUPS | tr ',' ' '); do
+    if /bin/egrep  -i "^$group" /etc/group ; then
+    else
+       groupadd $group
+    fi
+done
+usermod -a -G ${DEFGROUPS} ${USER}

e29aa22cbac1b94f5e0e2d954b958c7e6ceda03a
Sietze van Buuren <s.van.buuren@gmail.com>
2023-12-01 20:54:27 +0100

    Fix setup_user_groups script
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/setup_user_groups.sh
+++ scripts/setup_user_groups.sh
@@ -2,8 +2,7 @@
 USER="{{username}}"
 DEFGROUPS="{{groups}}"
 for group in $(echo $DEFGROUPS | tr ',' ' '); do
-    if /bin/egrep  -i "^$group" /etc/group ; then
-    else
+    if ! /bin/egrep  -i "^$group" /etc/group ; then
        groupadd $group
     fi
 done

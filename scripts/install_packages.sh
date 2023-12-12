048040f86b99f81e940c59caf4cdedc526bd13ce
Sietze van Buuren <s.van.buuren@gmail.com>
2023-12-12 19:31:00 +0100

    Support lists for install packages script
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apt-get -y update
-apt-get install --no-install-recommends -y {{packages}}
+apt-get install --no-install-recommends -y {{ packages if packages is string else " ".join(packages) }}

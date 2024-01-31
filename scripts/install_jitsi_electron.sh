b7564221c41d000ce2c3dee5727bd35915ef60e3
Sietze van Buuren <s.van.buuren@gmail.com>
2024-01-31 20:40:39 +0100

    Remove jisti debian file after installation has finished
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_jitsi_electron.sh
+++ scripts/install_jitsi_electron.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 wget https://github.com/jitsi/jitsi-meet-electron/releases/latest/download/jitsi-meet-amd64.deb
 sudo apt-get install -y ./jitsi-meet-amd64.deb
+rm -rf ./jitsi-meet-amd64.deb

b03ffee36d94d4751e46a5ac0420381554af2497
Sietze van Buuren <s.van.buuren@gmail.com>
2024-02-14 20:17:35 +0100

    Improve virtualenvwrapper script
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/configure_virtualenvwrapper.sh
+++ scripts/configure_virtualenvwrapper.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-BASHRC=$HOME/.bashrc;
-VENV=$HOME/.venv;
-mkdir -p $VENV;
-echo "\nexport WORKON_HOME=$VENV\n" >> $BASHRC;
-echo ". /usr/share/virtualenvwrapper/virtualenvwrapper.sh\n" >> $BASHRC;
+HOME_DIR=/home/{{username}}
+BASHRC=${HOME_DIR}/.bashrc
+VENV=${HOME_DIR}/.venv
+mkdir -p $VENV
+cat >> $BASHRC << EOL
+
+export WORKON_HOME=$VENV
+. /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+
+EOL

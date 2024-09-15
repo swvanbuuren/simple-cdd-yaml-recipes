c57a8a4be9a73538ec20264da55d7502aba27b2f
Sietze van Buuren <s.van.buuren@gmail.com>
2024-09-15 11:45:22 +0200

    Move to Skype flatpak installation
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_skype.sh
+++ scripts/install_skype.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-mkdir -p /etc/apt/keyrings
-curl -sS https://repo.skype.com/data/SKYPE-GPG-KEY \
-     | gpg --dearmor --yes -o /etc/apt/keyrings/skype-stable.gpg
-echo "deb [ arch=amd64, signed-by=/etc/apt/keyrings/skype-stable.gpg ] https://repo.skype.com/deb stable main" \
-    | tee /etc/apt/sources.list.d/skype-stable.list
+USER={{username}}
 apt-get -y update
-apt-get install --no-install-recommends -y skypeforlinux
+apt-get install --no-install-recommends -y flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo -H -u ${USER} flatpak install flathub com.skype.Client

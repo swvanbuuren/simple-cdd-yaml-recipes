afc495a4844d56c5dd932087452e81fa25274c45
Sietze van Buuren <s.van.buuren@gmail.com>
2025-02-25 15:03:22 +0100

    Add script to install official firefox version
    Firefox now offers an official Debian package. This replaces the latest version as Debian package as offered by Ubuntu.
    
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

---
+++ scripts/install_firefox.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir -p /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- \
+    | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" \
+    | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+apt-get -y update
+apt-get install --no-install-recommends -y firefox

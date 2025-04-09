7e48935da837bc3447911c659f698db19ed7c200
Sietze van Buuren <s.van.buuren@gmail.com>
2025-04-09 19:52:02 +0200

    Use https for spotify apt repository
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_spotify.sh
+++ scripts/install_spotify.sh
@@ -2,7 +2,7 @@
 mkdir -p /etc/apt/keyrings
 curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg \
     | gpg --dearmor --yes -o /etc/apt/keyrings/spotify.gpg
-echo "deb [ signed-by=/etc/apt/keyrings/spotify.gpg ] http://repository.spotify.com stable non-free" \
+echo "deb [ signed-by=/etc/apt/keyrings/spotify.gpg ] https://repository.spotify.com stable non-free" \
     | tee /etc/apt/sources.list.d/spotify.list
 apt-get -y update
 apt-get install --no-install-recommends -y spotify-client

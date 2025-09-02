889308bf6912d5dfee5448e50baddce07d1af421
Sietze van Buuren <s.van.buuren@gmail.com>
2025-09-02 19:11:13 +0200

    Fix snes9x install script
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_snes9x.sh
+++ scripts/install_snes9x.sh
@@ -4,6 +4,7 @@ apt-get install --no-install-recommends -y fuse3
 mkdir -p /home/${USERNAME}/.local/bin
 wget -O /home/${USERNAME}/.local/bin/Snes9x.AppImage https://github.com/snes9xgit/snes9x/releases/download/1.62.3/Snes9x-1.62.3-x86_64.AppImage
 chmod +x /home/${USERNAME}/.local/bin/Snes9x.AppImage
+mkdir -p /home/${USERNAME}/.local/share/applications
 wget -O /home/${USERNAME}/.local/share/applications/snes9x_logo.png https://upload.wikimedia.org/wikipedia/commons/b/bd/SNES9X_logo.png
 cat > /home/${USERNAME}/.local/share/applications/Snes9x.dekstop << EOL
 [Desktop Entry]

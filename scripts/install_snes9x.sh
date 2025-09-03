bef8bcf4dc561f414c3b82b29cbd397ecda2676e
Sietze van Buuren <s.van.buuren@gmail.com>
2025-09-03 20:16:01 +0200

    Update Snes9x AppImage version
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_snes9x.sh
+++ scripts/install_snes9x.sh
@@ -2,7 +2,7 @@
 USERNAME={{username}}
 apt-get install --no-install-recommends -y fuse3 libfuse2t64 libglibmm-2.4-1t64
 mkdir -p /home/${USERNAME}/.local/bin
-wget -O /home/${USERNAME}/.local/bin/Snes9x.AppImage https://github.com/snes9xgit/snes9x/releases/download/1.62.3/Snes9x-1.62.3-x86_64.AppImage
+wget -O /home/${USERNAME}/.local/bin/Snes9x.AppImage https://github.com/snes9xgit/snes9x/releases/download/1.63/Snes9x-1.63-x86_64.AppImage
 chmod +x /home/${USERNAME}/.local/bin/Snes9x.AppImage
 mkdir -p /home/${USERNAME}/.local/share/applications
 wget -O /home/${USERNAME}/.local/share/applications/snes9x_logo.png https://upload.wikimedia.org/wikipedia/commons/b/bd/SNES9X_logo.png

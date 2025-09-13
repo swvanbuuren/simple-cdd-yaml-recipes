1a46cf8a3138ed61d96e7a4a8f253d25b0c8b974
Sietze van Buuren <s.van.buuren@gmail.com>
2025-09-13 11:09:33 +0200

    Make sure git is installed for Greybird theme installation
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_greybird_theme.sh
+++ scripts/install_greybird_theme.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 REQUIRED_PACKAGES="autoconf libgdk-pixbuf-2.0-dev libglib2.0-bin librsvg2-dev meson ruby-sass sassc"
 apt-get -y update
-apt-get install --no-install-recommends -y $REQUIRED_PACKAGES
+apt-get install --no-install-recommends -y git $REQUIRED_PACKAGES
 git clone https://github.com/shimmerproject/Greybird.git
 cd Greybird
 meson --prefix=/usr builddir

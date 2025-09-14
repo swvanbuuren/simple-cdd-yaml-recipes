e64c3a46d4ee1e38b9efd25959af2143867de07b
Sietze van Buuren <s.van.buuren@gmail.com>
2025-09-14 19:51:47 +0200

    Make sure config is done for user
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/force_qt_platform_theme.sh
+++ scripts/force_qt_platform_theme.sh
@@ -4,6 +4,8 @@ apt-get install --no-install-recommends -y qt5-gtk-platformtheme wget
 # see https://forums.bunsenlabs.org/viewtopic.php?id=9322&p=4
 wget https://download.opensuse.org/repositories/home:/unit193/Debian_13/amd64/qt5gtk2_1.0-0vanir1_amd64.deb
 apt-get install -y ./qt5gtk2_1.0-0vanir1_amd64.deb
+su - {{username}} << 'EOF'
 touch $HOME/.xsessionrc
 echo "# Qt should respect platform theme" >> $HOME/.xsessionrc
 echo "export QT_QPA_PLATFORMTHEME=qt5gtk2" >> $HOME/.xsessionrc
+EOF

12331b481a05bc7fa2b7c598ae3be7059d680856
Sietze van Buuren <s.van.buuren@gmail.com>
2025-09-14 18:06:10 +0200

    Make sure wget is installed
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/force_qt_platform_theme.sh
+++ scripts/force_qt_platform_theme.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update
-apt-get install --no-install-recommends -y qt5-gtk-platformtheme
+apt-get install --no-install-recommends -y qt5-gtk-platformtheme wget
 # see https://forums.bunsenlabs.org/viewtopic.php?id=9322&p=4
 wget https://download.opensuse.org/repositories/home:/unit193/Debian_13/amd64/qt5gtk2_1.0-0vanir1_amd64.deb
 apt-get install -y ./qt5gtk2_1.0-0vanir1_amd64.deb

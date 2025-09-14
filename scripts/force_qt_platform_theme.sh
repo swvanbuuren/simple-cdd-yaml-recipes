6d57be0d9c519ba3428a69e13589384c88c6b1a5
Sietze van Buuren <s.van.buuren@gmail.com>
2025-09-14 17:01:12 +0200

    Add comment to qt theme script
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/force_qt_platform_theme.sh
+++ scripts/force_qt_platform_theme.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt-get update
 apt-get install --no-install-recommends -y qt5-gtk-platformtheme
+# see https://forums.bunsenlabs.org/viewtopic.php?id=9322&p=4
 wget https://download.opensuse.org/repositories/home:/unit193/Debian_13/amd64/qt5gtk2_1.0-0vanir1_amd64.deb
 apt-get install -y ./qt5gtk2_1.0-0vanir1_amd64.deb
 touch $HOME/.xsessionrc

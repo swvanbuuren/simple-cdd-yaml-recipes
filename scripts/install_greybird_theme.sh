e0eb8c88e67de98e2359ca682ee1abb9766065b4
Sietze van Buuren <s.van.buuren@gmail.com>
2025-09-14 13:13:26 +0200

    Greybird theme installation only removes packages that were added for installation
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_greybird_theme.sh
+++ scripts/install_greybird_theme.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-REQUIRED_PACKAGES="autoconf libgdk-pixbuf-2.0-dev libglib2.0-bin librsvg2-dev meson ruby-sass sassc"
+REQUIRED_PACKAGES="git autoconf libgdk-pixbuf-2.0-dev libglib2.0-bin librsvg2-dev meson ruby-sass sassc"
 apt-get -y update
-apt-get install --no-install-recommends -y git $REQUIRED_PACKAGES
+NEWLY_INSTALLED_PACKAGES=$(apt-get install --simulate $REQUIRED_PACKAGES \
+| awk '
+/^The following NEW packages will be installed:/ {c = 1; next}
+c && /^[[:space:]]/ { gsub(/^[[:space:]]+|[[:space:]]+$/, ""); s = s ? s " " $0 : $0; next }
+c { if (s) { print s; c = 0 } exit }
+END { if (c && s) print s }
+')
+apt-get install --no-install-recommends -y $REQUIRED_PACKAGES
 git clone https://github.com/shimmerproject/Greybird.git
 cd Greybird
 meson --prefix=/usr builddir
@@ -10,5 +17,4 @@ ninja
 ninja install
 cd ..
 rm -rf Greybird
-apt-get purge -y $REQUIRED_PACKAGES
-apt-get autoremove -y
+apt-get purge -y $NEWLY_INSTALLED_PACKAGES

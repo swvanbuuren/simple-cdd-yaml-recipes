724e5bce92117a2276b0eb73236aecf8b17e1320
Sietze van Buuren <s.van.buuren@gmail.com>
2025-09-20 15:26:27 +0200

    Fix setup uv script
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/setup_uv.sh
+++ scripts/setup_uv.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # NOTE: this script should be run by the end user (i.e. NOT root)
+su - {{username}} << 'EOF'
 curl -LsSf https://astral.sh/uv/install.sh | sh
 echo 'eval "$(uv generate-shell-completion bash)"' >> ${HOME}/.bashrc
 echo 'eval "$(uvx --generate-shell-completion bash)"' >> ~/.bashrc
+EOF

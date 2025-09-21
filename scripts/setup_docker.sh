dc25a0aed73501448cf4674b1582a168ceafd3d6
Sietze van Buuren <s.van.buuren@gmail.com>
2025-09-21 14:01:39 +0200

    Add docker-cli to docker setup script
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 USERNAME={{username}}
 apt-get update
-apt-get install --no-install-recommends -y docker.io
+apt-get install --no-install-recommends -y docker.io docker-cli
 groupadd docker
 usermod -aG docker $USERNAME

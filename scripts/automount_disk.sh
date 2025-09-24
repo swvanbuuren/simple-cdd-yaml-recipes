62d250f7bfac19e85794eb32abf2f959fed150b4
Sietze van Buuren <s.van.buuren@gmail.com>
2025-09-24 17:19:34 +0200

    Add disk type support to automount script
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/automount_disk.sh
+++ scripts/automount_disk.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 UUID={{uuid}}
 MOUNT_POINT={{mount_point}}
+DISK_TYPE={{ disk_type | default("ext4") }}
+
 mkdir -p $MOUNT_POINT
-echo "UUID=${UUID}       ${MOUNT_POINT}            ext4    defaults,errors=remount-ro,nofail       0       1" >> /etc/fstab
+
+case "$DISK_TYPE" in
+    ext4)
+        fstype="ext4"
+        mount_opts="defaults,errors=remount-ro,nofail"
+        dump=0
+        pass=1
+        ;;
+    ntfs)
+        fstype="ntfs-3g"
+        mount_opts="rw,uid=1000,gid=1000,dmask=0002,fmask=0003"
+        dump=0
+        pass=0
+        ;;
+
+echo "UUID=${UUID}       ${MOUNT_POINT}            ${fstype}    ${mount_opts}       ${dump}       ${pass}" >> /etc/fstab

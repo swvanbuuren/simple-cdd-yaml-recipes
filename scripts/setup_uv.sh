01300df6759dab66deb69f59bc88b24cc552f401
Sietze van Buuren <s.van.buuren@gmail.com>
2025-11-25 18:51:22 +0100

    Remove install step from setup uv script
    For installation use install_uv.sh instead
    
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/setup_uv.sh
+++ scripts/setup_uv.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # NOTE: this script should be run by the end user (i.e. NOT root)
 su - {{username}} << 'EOF'
-curl -LsSf https://astral.sh/uv/install.sh | sh
 echo '# uv settings' >> ${HOME}/.bashrc
 echo 'eval "$(uv generate-shell-completion bash)"' >> ${HOME}/.bashrc
 echo 'eval "$(uvx --generate-shell-completion bash)"' >> ${HOME}/.bashrc

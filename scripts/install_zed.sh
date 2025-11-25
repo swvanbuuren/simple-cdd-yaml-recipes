532495b1d5d4e25a798816c24ae26fd33eae6e19
Sietze van Buuren <s.van.buuren@gmail.com>
2025-11-25 19:41:24 +0100

    Add required package for zed install
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_zed.sh
+++ scripts/install_zed.sh
@@ -2,4 +2,4 @@
 curl -sS https://debian.griffo.io/EA0F721D231FDD3A0A17B9AC7808B4DD62C41256.asc | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/debian.griffo.io.gpg
 echo "deb https://debian.griffo.io/apt $(lsb_release -sc 2>/dev/null) main" | tee /etc/apt/sources.list.d/debian.griffo.io.list
 apt-get update
-apt-get install --no-install-recommends -y zed
+apt-get install --no-install-recommends -y mesa-vulkan-drivers zed

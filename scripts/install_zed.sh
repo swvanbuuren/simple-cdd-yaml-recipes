fd925d9d1de137ec794ba834ac28b62ad4f42e16
Sietze van Buuren <s.van.buuren@gmail.com>
2025-11-26 19:14:53 +0100

    Update zed install script to include trixie-backport package update
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_zed.sh
+++ scripts/install_zed.sh
@@ -3,3 +3,13 @@ curl -sS https://debian.griffo.io/EA0F721D231FDD3A0A17B9AC7808B4DD62C41256.asc |
 echo "deb https://debian.griffo.io/apt $(lsb_release -sc 2>/dev/null) main" | tee /etc/apt/sources.list.d/debian.griffo.io.list
 apt-get update
 apt-get install --no-install-recommends -y mesa-vulkan-drivers zed
+
+# if we're on trixie add trixie-backports and update from there
+. /etc/os-release
+if [ "$VERSION_CODENAME" = "trixie" ]; then
+    BACKPORT_LINE="deb http://deb.debian.org/debian/ trixie-backports main contrib non-free-firmware non-free"
+    SOURCES_FILE="/etc/apt/sources.list.d/trixie-backports.list"
+    echo "$BACKPORT_LINE" | tee "$SOURCES_FILE" > /dev/null
+    apt-get update
+    apt-get install --no-install-recommends -y mesa-vulkan-drivers/trixie-backports
+fi

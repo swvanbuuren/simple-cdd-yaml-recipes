dacfb24d5ab59dc885d102ef70d45905be7a6889
Sietze van Buuren <s.van.buuren@gmail.com>
2025-12-06 09:34:55 +0100

    Remove unneeded sudo's
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_vscode.sh
+++ scripts/install_vscode.sh
@@ -6,6 +6,6 @@ install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packa
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" \
     | tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
-sudo apt-get install --no-install-recommends -y apt-transport-https
-sudo apt-get -y update
-sudo apt-get install --no-install-recommends -y code
+apt-get install --no-install-recommends -y apt-transport-https
+apt-get -y update
+apt-get install --no-install-recommends -y code

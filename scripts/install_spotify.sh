e5017de45ff174e33fd969c9d68a832fe59f1427
Sietze van Buuren <s.van.buuren@gmail.com>
2026-02-21 10:59:57 +0100

    Update public key in spotify install script
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- scripts/install_spotify.sh
+++ scripts/install_spotify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p /etc/apt/keyrings
-curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg \
+curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc \
     | gpg --dearmor --yes -o /etc/apt/keyrings/spotify.gpg
 echo "deb [ signed-by=/etc/apt/keyrings/spotify.gpg ] https://repository.spotify.com stable non-free" \
     | tee /etc/apt/sources.list.d/spotify.list

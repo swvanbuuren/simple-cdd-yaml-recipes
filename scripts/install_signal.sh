b990d51b8abb5b1e03fac380477770e44da8af4e
Sietze van Buuren <s.van.buuren@gmail.com>
2026-05-18 16:32:43 +0200

    Add signal-desktop installation script
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

---
+++ scripts/install_signal.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# NOTE: These instructions only work for 64-bit Debian-based
+# Linux distributions such as Ubuntu, Mint etc.
+
+# 1. Install our official public software signing key:
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+# 2. Add our repository to your list of repositories:
+wget -O signal-desktop.sources https://updates.signal.org/static/desktop/apt/signal-desktop.sources
+cat signal-desktop.sources | tee /etc/apt/sources.list.d/signal-desktop.sources > /dev/null
+
+# 3. Update your package database and install Signal:
+apt-get update && apt-get install -y signal-desktop
